aa10d89eb23bbdda619ee3e116f89f0031a9feaa
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-10-13 22:31:53 +0300

    Updates

---
+++ precache-manifest.8835793b0bb72be5ba4512372a810851.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3aa46f8fc374a2b2de1bdfa2083a2d5",
+    "url": "/japan-spirit/index.html"
+  },
+  {
+    "revision": "f63b759e52e76c2f69b3",
+    "url": "/japan-spirit/static/css/2.2a86faee.chunk.css"
+  },
+  {
+    "revision": "f41309aa6d1d007f8862",
+    "url": "/japan-spirit/static/css/main.3d9588f1.chunk.css"
+  },
+  {
+    "revision": "f63b759e52e76c2f69b3",
+    "url": "/japan-spirit/static/js/2.719f2b99.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/japan-spirit/static/js/2.719f2b99.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f41309aa6d1d007f8862",
+    "url": "/japan-spirit/static/js/main.9d2c305b.chunk.js"
+  },
+  {
+    "revision": "3853542d82439a699c41",
+    "url": "/japan-spirit/static/js/runtime-main.5f2e53e0.js"
+  }
+]);
