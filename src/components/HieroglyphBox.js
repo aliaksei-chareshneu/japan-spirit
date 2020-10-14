ef3603b53e2cd185e140e4d0611c865b9c745647
aliaksei-chareshneu <chareshneu.tech@gmail.com>
2020-10-14 11:42:51 +0300

    Refactor HieroglyphBox; improve bg img retrieval
    Now background image url is a part of state
    State is updated only onmount

--- src/components/HieroglyphBox.js
+++ src/components/HieroglyphBox.js
@@ -4,48 +4,75 @@ import { Container, Row, Col, Button } from 'react-bootstrap'
 // import dict from '../../node_modules/japanese-json/kana.json'
 
 const HieroglyphBox = () => {
-    const [hieroglyph, setHieroglyph] = useState(null)
+    const [hieroglyphEntry, setHieroglyphEntry] = useState(null)
+    const [backgroundImageUrl, setBackgroundImageUrl] = useState(null)
 
-    useEffect(() => {
+    const toggleHintOpacity = () => {
+        const hint = document.getElementById("hint")
+        hint.style.opacity === "1" ? hint.style.opacity = "0" : hint.style.opacity = "1"
+    }
+
+    const loadNewHieroglyphEntry = () => {
         fetch("https://kanjiapi.dev/v1/kanji/grade-1")
-            .then(response => response.json())
+            .then(r => r.json())
             .then(data => {
-                const randomInt = getRandomInt(1, data.length)
-                fetch("https://kanjiapi.dev/v1/kanji/" + data[randomInt])
-                    .then(response => response.json())
+                fetch("https://kanjiapi.dev/v1/kanji/" + data[getRandomInt(1, data.length)])
+                    .then(r => r.json())
                     .then(data => {
-                        setHieroglyph(data)
+                        setHieroglyphEntry(data)
                         console.log(data)
                     })
             })
+    }
+
+    const loadNewBackgroundImage = () => {
+        fetch("https://source.unsplash.com/collection/1252289")
+            .then(r => {
+                setBackgroundImageUrl(r.url)
+                console.log(r.url)
+            })
+    }
+
+    useEffect(() => {
+        loadNewHieroglyphEntry()
+        loadNewBackgroundImage()
     }, [])
+
+    // useEffect(() => {
+    //     fetch("https://kanjiapi.dev/v1/kanji/grade-1")
+    //         .then(response => response.json())
+    //         .then(data => {
+    //             const randomInt = getRandomInt(1, data.length)
+    //             fetch("https://kanjiapi.dev/v1/kanji/" + data[randomInt])
+    //                 .then(response => response.json())
+    //                 .then(data => {
+    //                     setHieroglyph(data)
+    //                     console.log(data)
+    //                 })
+    //         })
+    // }, [])
 
 
     return (
-        <main id="box" className="text-center vertical-center text-white">
+        <main id="box" className="text-center vertical-center text-white" style={{backgroundImage: `url(${backgroundImageUrl})`}}>
             <Container fluid style={{ maxWidth: "600px" }}>
 
                 <article className="card">
                     <div className="card-body">
                         <Row>
                             <Col>
-                                <h1 lang="ja-jp" className="p-1 display-1">{hieroglyph ? hieroglyph.kanji : "..."}</h1>
-                                <h2 id="hint" style={{ opacity: "0" }} className="p-1 mb-5">{hieroglyph ? hieroglyph.heisig_en : "Загружается..."}</h2>
+                                <h1 lang="ja-jp" className="p-1 display-1">{hieroglyphEntry ? hieroglyphEntry.kanji : "..."}</h1>
+                                <h2 id="hint" style={{ opacity: "0" }} className="p-1 mb-5">{hieroglyphEntry ? hieroglyphEntry.heisig_en : "..."}</h2>
                                 {/* <blockquote>{quotes ? quotes[randomInt].text : "Fetching random quote ..."}</blockquote>
                                 <div>{quotes ? quotes[randomInt].author : "Fetching random quote author ..."}</div>*/}
                             </Col>
                         </Row>
                         <Row>
                             <Col>
-                                <button className="btn btn-lg btn-secondary btn-block mb-2" onClick={() => {
-                                    document.getElementById("hint").style.opacity = "1"
-                                }}>Подсказка?</button>
+                                <button className="btn btn-lg btn-secondary btn-block mb-2" onClick={toggleHintOpacity}>Подсказка?</button>
                             </Col>
                             <Col>
-                                <button className="btn btn-lg btn-secondary btn-block mb-2" onClick={() => {
-                                    window.location.reload(true)
-                                }}>Новый иероглиф?</button>
-
+                                <button className="btn btn-lg btn-secondary btn-block mb-2" onClick={loadNewHieroglyphEntry, loadNewBackgroundImage}>Новый иероглиф?</button>
                             </Col>
                         </Row>
                     </div>
